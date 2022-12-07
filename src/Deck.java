d0feea88c5d4b490578fcdd540e15c6e6cdd236e
octavemoha <octave.moha@menloschool.org>
2022-12-07 11:20:34 -0800

    initial commit

---
+++ src/Deck.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Deck {
+
+    //instance variable for cards left
+    private int cardsLeft;
+
+    //array for cards
+    private ArrayList<Card> cards;
+    private ArrayList<Card> ranks;
+    private ArrayList<Card> suit;
+    private ArrayList<Card> pi;
+
+
+
+    public Deck(){
+
+    }
+
+}
